98918edaa987a8427d32a33b79c7d77efd68e6fe
18306043086 <1125213018@qq.com>
2015-04-01 18:43:56 +0800

    分类 商品详情 zyz

---
+++ art/src/com/shangxian/art/CommodityContentActivity.java
@@ -0,0 +1,15 @@
+package com.shangxian.art;
+
+import android.os.Bundle;
+
+import com.shangxian.art.base.BaseActivity;
+
+public class CommodityContentActivity extends BaseActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_commoditycontent);
+	}
+}
--- art/src/com/shangxian/art/bean/ClassityCommdityModel.java
+++ art/src/com/shangxian/art/bean/ClassityCommdityModel.java
@@ -1,5 +1,50 @@
 package com.shangxian.art.bean;
 
-public class ClassityCommdityModel {
+import java.io.Serializable;
 
+public class ClassityCommdityModel implements Serializable{
+
+	private String title;
+	private String summary;
+	private String price;
+	private String img;
+	private String shop;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getShop() {
+		return shop;
+	}
+	public void setShop(String shop) {
+		this.shop = shop;
+	}
+	@Override
+	public String toString() {
+		return "ClassityCommdityModel [title=" + title + ", summary=" + summary
+				+ ", price=" + price + ", img=" + img + ", shop=" + shop + "]";
+	}
+	
+	
 }
