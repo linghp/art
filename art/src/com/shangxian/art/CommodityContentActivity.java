892545106a40bb19460e540c8bf4980dc01141c5
18306043086 <1125213018@qq.com>
2015-04-02 16:39:26 +0800

    商品信息完善

--- art/src/com/shangxian/art/CommodityContentActivity.java
+++ art/src/com/shangxian/art/CommodityContentActivity.java
@@ -3,13 +3,35 @@ package com.shangxian.art;
 import android.os.Bundle;
 
 import com.shangxian.art.base.BaseActivity;
+import com.shangxian.art.view.StarRatingView;
 
 public class CommodityContentActivity extends BaseActivity{
 
+//	private StarRatingView star;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_commoditycontent);
+		
+		initView();
+		initData();
+		listener();
+	}
+
+	private void listener() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initView() {
+		// TODO Auto-generated method stub
+//		star = (StarRatingView) findViewById(R.id.commoditycontent_starRating);
+//		star.setSelectNums(1);//设置默认选中星星数
 	}
 }
