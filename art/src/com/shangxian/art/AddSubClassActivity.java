14b52c110b790f30f52bf20bf4e0fbb4ee50e875
18306043086 <1125213018@qq.com>
2015-04-07 18:21:55 +0800

    商品类别维护

--- art/src/com/shangxian/art/AddSubClassActivity.java
+++ art/src/com/shangxian/art/AddSubClassActivity.java
@@ -1,6 +1,9 @@
 package com.shangxian.art;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.shangxian.art.base.BaseActivity;
@@ -12,8 +15,8 @@ import com.shangxian.art.base.BaseActivity;
  *
  */
 public class AddSubClassActivity extends BaseActivity{
-	TextView xuanze,addimg,addname,quxiao,baocun;
-	
+	TextView xuanze,addname,quxiao,baocun;
+	ImageView addimg;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -27,17 +30,59 @@ public class AddSubClassActivity extends BaseActivity{
 	private void initView() {
 		// TODO Auto-generated method stub
 		xuanze = (TextView) findViewById(R.id.addsubclass_xuanze);
-		addimg = (TextView) findViewById(R.id.addsubclass_addimg);
+		addimg = (ImageView) findViewById(R.id.addsubclass_addimg);
+		addname = (TextView) findViewById(R.id.addsubclass_addname);
+		quxiao = (TextView) findViewById(R.id.addsubclass_quxiao);
+		baocun = (TextView) findViewById(R.id.addsubclass_baocun);
 	}
 
 	private void initData() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void initListener() {
 		// TODO Auto-generated method stub
-		
+		xuanze.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				System.out.println(">>>>>>>>>>选择父类");
+			}
+		});
+		addimg.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				System.out.println(">>>>>>>>>>添加图片");
+			}
+		});
+		addname.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				System.out.println(">>>>>>>>>>添加分类名称");
+			}
+		});
+		quxiao.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				System.out.println(">>>>>>>>>>取消");
+			}
+		});
+		baocun.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				System.out.println(">>>>>>>>>>保存");
+			}
+		});
 	}
 
 }
