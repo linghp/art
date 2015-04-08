21559b4bd27d4a866b5303b7985abeb5551004bb
linghp <421620330@qq.com>
2015-04-08 10:15:34 +0800

    xxx

--- art/src/com/shangxian/art/MineActivity.java
+++ art/src/com/shangxian/art/MineActivity.java
@@ -110,7 +110,7 @@ public class MineActivity extends BaseActivity {
 		if(islogin()){
         String userphoto_filename_temp = LocalUserInfo.getInstance(this)
                 .getUserInfo(LocalUserInfo.USERPHOTO_FILENAME);
-        if (!userphoto_filename_temp.equals(userphoto_filename)) {
+       // if (!userphoto_filename_temp.equals(userphoto_filename)) {
         	String imagelocaldir=AbFileUtil.getImageDownloadDir(this)+File.separator;
     		if (!TextUtils.isEmpty(userphoto_filename_temp)) {
     			File file = new File(imagelocaldir, userphoto_filename_temp);
@@ -121,7 +121,7 @@ public class MineActivity extends BaseActivity {
     						+ userphoto_filename_temp));
     			}
     		}
-        }
+      //  }
 		}
 	}
 	
--- art/src/com/shangxian/art/base/BaseActivity.java
+++ art/src/com/shangxian/art/base/BaseActivity.java
@@ -1,6 +1,11 @@
 package com.shangxian.art.base;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.ab.activity.AbActivity;
@@ -18,5 +23,46 @@ public class BaseActivity extends AbActivity {
 
 	protected void myToast(String str) {
 		Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+	}
+	
+	//触摸其他区域，输入法界面消失
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev) {
+		if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+			View v = getCurrentFocus();
+			if (isShouldHideInput(v, ev)) {
+
+				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+				if (imm != null) {
+					imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+				}
+			}
+			return super.dispatchTouchEvent(ev);
+		}
+		// 必不可少，否则所有的组件都不会有TouchEvent了
+		if (getWindow().superDispatchTouchEvent(ev)) {
+			return true;
+		}
+		return onTouchEvent(ev);
+	}
+	
+	public  boolean isShouldHideInput(View v, MotionEvent event) {
+		if (v != null && (v instanceof EditText)) {
+			int[] leftTop = { 0, 0 };
+			//获取输入框当前的location位置
+			v.getLocationInWindow(leftTop);
+			int left = leftTop[0];
+			int top = leftTop[1];
+			int bottom = top + v.getHeight();
+			int right = left + v.getWidth();
+			if (event.getX() > left && event.getX() < right
+					&& event.getY() > top && event.getY() < bottom) {
+				// 点击的是输入框区域，保留点击EditText的事件
+				return false;
+			} else {
+				return true;
+			}
+		}
+		return false;
 	}
 }
