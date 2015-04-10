a5710a2f778307f8411848d857d464463e2f1372
18306043086 <1125213018@qq.com>
2015-04-10 14:28:53 +0800

    zyz

--- art/src/com/shangxian/art/ShopsActivity.java
+++ art/src/com/shangxian/art/ShopsActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.PopupWindow.OnDismissListener;
 
+import com.ab.util.dct.IFDCT;
 import com.shangxian.art.base.BaseActivity;
 import com.shangxian.art.utils.CommonUtil;
 import com.shangxian.art.view.TopView;
@@ -20,10 +22,12 @@ import com.shangxian.art.view.TopView;
  *
  */
 public class ShopsActivity extends BaseActivity{
-
+	ImageView img,shopsimg,collectionimg,img1,img2;
+	TextView shopsname,guanzu,all,up,youhui,summary1,price1,summary2,price2;
 	
+	//判断是否收藏
+	boolean iscollection = false;
 	//popupwindow
-	
 	private PopupWindow popupWindow;
 	private View view;
 	private TextView fenlei,jianjie;
@@ -45,6 +49,22 @@ public class ShopsActivity extends BaseActivity{
 		topView.setRightBtnDrawable(R.drawable.more1);
 		topView.showCenterSearch();
 		topView.setBack(R.drawable.back);//返回
+		
+		img = (ImageView) findViewById(R.id.shops_img);
+		shopsimg = (ImageView) findViewById(R.id.shops_shopsimg);
+		collectionimg = (ImageView) findViewById(R.id.shops_collectionimg);
+		img1 = (ImageView) findViewById(R.id.shops_img1);
+		img2 = (ImageView) findViewById(R.id.shops_img2);
+		
+		shopsname= (TextView) findViewById(R.id.shops_shopsname);
+		guanzu= (TextView) findViewById(R.id.shops_guanzu);
+		all= (TextView) findViewById(R.id.shops_all);
+		up= (TextView) findViewById(R.id.shops_up);
+		youhui= (TextView) findViewById(R.id.shops_youhui);
+		summary1= (TextView) findViewById(R.id.shops_summary1);
+		price1= (TextView) findViewById(R.id.shops_price1);
+		summary2= (TextView) findViewById(R.id.shops_summary2);
+		price2= (TextView) findViewById(R.id.shops_price2);
 	}
 
 	private void initData() {
@@ -60,6 +80,22 @@ public class ShopsActivity extends BaseActivity{
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				popupWindow.showAsDropDown(topView, 460, -30);
+			}
+		});
+		//收藏
+		collectionimg.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (!iscollection) {
+					collectionimg.setImageResource(R.drawable.collection_on);
+					myToast("已收藏");
+				}else {
+					collectionimg.setImageResource(R.drawable.collection_off);
+					myToast("取消收藏");
+				}
+				iscollection = !iscollection;
 			}
 		});
 	}
--- art/src/com/shangxian/art/ShopsSummaryActivity.java
+++ art/src/com/shangxian/art/ShopsSummaryActivity.java
@@ -1,6 +1,12 @@
 package com.shangxian.art;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.shangxian.art.base.BaseActivity;
 import com.shangxian.art.view.TopView;
@@ -10,6 +16,9 @@ import com.shangxian.art.view.TopView;
  *
  */
 public class ShopsSummaryActivity extends BaseActivity{
+	ImageView shopsimg,phoneimg,addressimg;//商铺图片、电话图片、地址图片
+	TextView shopsname,guanzu,zhanggui,phone,address,summary;//商铺名字、关注、掌柜名、电话、地址、简介
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -24,9 +33,20 @@ public class ShopsSummaryActivity extends BaseActivity{
 		// TODO Auto-generated method stub
 		topView=(TopView) findViewById(R.id.top_title);
 		topView.setActivity(this);
-		topView.setRightBtnDrawable(R.drawable.more1);
+		topView.setRightBtnDrawable(R.drawable.collection);
 		topView.setTitle("商铺简介");
 		topView.setBack(R.drawable.back);//返回
+		
+		shopsimg = (ImageView) findViewById(R.id.shopssummary_shopsimg);
+		phoneimg = (ImageView) findViewById(R.id.shopssummary_phoneimg);
+		addressimg = (ImageView) findViewById(R.id.shopssummary_addressimg);
+		
+		shopsname = (TextView) findViewById(R.id.shopssummary_shopsname);
+		guanzu = (TextView) findViewById(R.id.shopssummary_guanzu);
+		zhanggui = (TextView) findViewById(R.id.shopssummary_zhanggui);
+		phone = (TextView) findViewById(R.id.shopssummary_phone);
+		address = (TextView) findViewById(R.id.shopssummary_address);
+		summary = (TextView) findViewById(R.id.shopssummary_summary);
 	}
 
 	private void initData() {
@@ -36,7 +56,25 @@ public class ShopsSummaryActivity extends BaseActivity{
 
 	private void initListener() {
 		// TODO Auto-generated method stub
-		
+		//拨打电话
+		phoneimg.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:"+10086));
+				startActivity(intent);
+			}
+		});
+		//跳转到定位
+		addressimg.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 }
