597e2849e479150d14b4411ad6b9773414112d6b
18306043086 <1125213018@qq.com>
2015-05-04 17:13:03 +0800

    zyz

---
+++ art/src/com/shangxian/art/JinJiJieSuanActivity.java
@@ -0,0 +1,56 @@
+package com.shangxian.art;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import com.shangxian.art.base.BaseActivity;
+import com.shangxian.art.view.TopView;
+
+/**
+ * 紧急结算
+ * @author Administrator
+ *
+ */
+public class JinJiJieSuanActivity extends BaseActivity{
+
+	private TextView shenqing; 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_jinjijiesuan);
+		initView();
+		initData();
+		initListener();
+	}
+
+	private void initView() {
+		topView=(TopView) findViewById(R.id.top_title);
+		topView.setActivity(this);
+		topView.hideCenterSearch();//隐藏搜索框
+		topView.hideRightBtn_invisible();//隐藏右按钮
+		topView.showTitle();
+		topView.setTitle("紧急结算");
+		topView.setBack(R.drawable.back);//返回
+		shenqing = (TextView) findViewById(R.id.jinjijiesuan_btn);
+	}
+
+	private void initData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initListener() {
+		shenqing.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				myToast("功能暂未开放");
+				
+			}
+		});
+		
+	}
+}
