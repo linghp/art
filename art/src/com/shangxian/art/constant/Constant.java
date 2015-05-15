d48f016cc4a5ba2721d16576143af1a34f0c4745
linghp <421620330@qq.com>
2015-05-15 18:20:16 +0800

    冲突

--- art/src/com/shangxian/art/constant/Constant.java
+++ art/src/com/shangxian/art/constant/Constant.java
@@ -8,10 +8,10 @@ public class Constant {
 	public static final String CONTENT = "/api";
 	public static final String HOME = "/ads";//动态布局的数据
 	public static final String CATEGORYS = "/categorys";//分类
-	public static final String CART = "/cart";//购物车
-	public static final String ORDER = "/order/from/cart";//下订单
+	public static final String CART = "/order/from/cart";//购物车
+	public static final String ORDER = "/order";//下订单
 	public static final String ORDERS = "/orders";//我的订单
-//	public static final String SHOPSLIST = "/1/shops";//
+//	public static final String SHOPSLIST = "/1/shops";
 	
 	
 	
