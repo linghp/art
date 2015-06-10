171b8c5c582f7b8ef140732464ca8165f08fe234
liVincent <1043973279@qq.com>
2015-06-10 18:04:19 +0800

    libo

---
+++ art/src/com/shangxian/art/adapter/ExpressPopuAdapter.java
@@ -0,0 +1,45 @@
+package com.shangxian.art.adapter;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.shangxian.art.R;
+import com.shangxian.art.bean.ExpressInfo;
+
+public class ExpressPopuAdapter extends EntityAdapter<ExpressInfo> {
+
+	private int index;
+	
+	public ExpressPopuAdapter(Activity mAc, int layoutId,
+			List<ExpressInfo> dates) {
+		super(mAc, layoutId, dates);
+	}
+
+	public void changeIndex(int index){
+		this.index = index;
+		notifyDataSetChanged();
+	}
+	
+	@Override
+	public View initView(int position, View convertView, ViewGroup parent) {
+		TextView title = null;
+		if (convertView == null) {
+			convertView = inflater();
+			title = (TextView) convertView.findViewById(R.id.logistt_tv_title);
+			convertView.setTag(title);
+		} else {
+			title = (TextView) convertView.getTag();
+		}
+		if (position == index) {
+			convertView.setSelected(true);
+		} else {
+			convertView.setSelected(false);
+		}
+		title.setText(getItem(position).getName());
+		return convertView;
+	}
+}
